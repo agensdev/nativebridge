46035772b3bed64943138d48607d6c13a4f3c466
Christian Simonsen <Christian@agens.no>
2019-05-24 14:06:30 +0200

    async v av nativebridge

--- nativebridge.js
+++ nativebridge.js
@@ -1,42 +1,142 @@
 export const nativebridge = function(){
 
-    window.mainapp.nativebridge = this;
+    if(window.nativemainapp != undefined){
+        return window.nativemainapp.nativebridge
+    }
+
+    const BRIDGE_VERSION = "0.0.0"
+    const COMMANDS = {
+            REQUEST_LOGIN:"login",
+            REQUEST_VERSION:"version",
+            TEST_CONNECTIVITY:"test",
+            TEST_ERROR:"testError"
+    }
     
-    const VERSION = "0.0.0"
-    const WEBAPP_READY_ACTION_URL = 'ntmsg://webapp_booted'
-    const CLOSE_ACTION_URL = 'norsktippingmainapp://back_from_webapp'
-    const LOGIN_ACTION_URL = 'ntmsg://request_native_log_in'
+    const LEGACY_COMMANDS = {
+        WEBAPP_READY_ACTION_URL = 'ntmsg://webapp_booted',
+        CLOSE_ACTION_URL = 'norsktippingmainapp://back_from_webapp',
+        LOGIN_ACTION_URL = 'ntmsg://request_native_log_in'
+    }
+     
+    const ERRORS = {
+        NOT_NATIVE_CONTEXT:"The application is not running in a native context",
+        NOT_NATIVE_BRIDGE:"The current application dos not support nativebridge commands"
+    
+    }
+
+    let isAppNativeBridgeEnabled = (() =>{
+        //TODO: HVa med android?
+        return window.webkit.messageHandlers.nativebridge != undefined
+    })();
 
     let isNativeContext = (() =>{
-        //This function will eventually tell us if the native app is available.
-        return true
+        //TODO: Dette er ikke riktig test for dettte, trengs å utvides. 
+        return window.webkit.messageHandlers.nativebridge != undefined
     })();
    
+    let createHandlerId = () => { return `h_${Date.now()}${(Math.random() + 1).toString(36).substring(4)}`};
+    let handlers = {}
+    let appInterface = window.webkit.messageHandlers.nativebridge;
 
-    return {
+    var appVersion = null;
+    var token = null;
 
-        "version":VERSION,
+    const bridge =  {
 
-        "isNativeContext": isNativeContext,
+        "version":BRIDGE_VERSION,
+
+        // When the native app successfully completes a request.
+        "onResponse":(handlerId, data)=>{
+            let handle = handlers[handlerId];
+            handle.resolve(data);
+            delete handlers[handlerId];
+        },
+        
+        // When the native app is unable for what ever reason to complete a request.
+        "onError":(handlerId, err)=>{
+            let handle = handlers[handlerId];
+            handle.reject(new Error(err));
+            delete handlers[handlerId];
+        },
+
+        "app_version":() =>{    //nativebridge only 
+
+            if(isNativeContext){
+                if(appVersion){ return appVersion} // Version number is known.
+                // Se if we can get the version number from the url search string
+                let params = new URLSearchParams(window.location.search)
+                let version = params.has("version") ? params.get("version"):null
+                if(version){
+                    appVersion = version;
+                    return Promise.resolve(version);
+                } else if(isAppNativeBridgeEnabled){
+                    //We ask the app to provide the version number
+                    return new Promise((resolve,reject) =>{
+                        const handle = createHandlerId()
+                        handlers[handle] = {resolve, reject}
+                        appInterface.postMessage(JSON.stringify({callback:handle, cmd:COMMANDS.REQUEST_VERSION}))
+                    })
+                }
+            } 
+            
+            return Promise.reject(new Error(ERRORS.NOT_NATIVE_BRIDGE));
+        },
+
+        "isNativeContext": isAppNativeBridgeEnabled,
+
+
+        "test":() =>{   //nativebridge only 
+            if(isAppNativeBridgeEnabled){
+                return new Promise((resolve,reject) =>{
+                    const handle = createHandlerId()
+                    handlers[handle] = {resolve, reject}
+                    appInterface.postMessage(JSON.stringify({callback:handle, cmd:COMMANDS.TEST_CONNECTIVITY}))
+                })
+            }
+            return Promise.reject(new Error(ERRORS.NOT_NATIVE_BRIDGE));
+        },
+
+        "testErr":() =>{    //nativebridge only 
+            if(isAppNativeBridgeEnabled){
+                return new Promise((resolve,reject) =>{
+                    const handle = createHandlerId()
+                    handlers[handle] = {resolve, reject}
+                    appInterface.postMessage(JSON.stringify({callback:handle, cmd:COMMANDS.TEST_ERROR}))
+                })
+            }
+            return Promise.reject(new Error(ERRORS.NOT_NATIVE_BRIDGE));
+        },
 
         "requestLogin":() =>{
-            if(isNativeContext && window){
-                window.location.href = LOGIN_ACTION_URL;
+
+            if(isAppNativeBridgeEnabled){
+                return new Promise((resolve,reject) =>{
+                    const handle = createHandlerId()
+                    handlers[handle] = {resolve, reject}
+                    appInterface.postMessage(JSON.stringify({callback:handle, cmd:COMMANDS.LOGIN}))
+                })
+            } else if(isNativeContext && window){
+                window.location.href = LEGACY_COMMANDS.LOGIN_ACTION_URL;
             }
+
+            return new Promise().resolve(null)
         },
 
         "signalReadyState":() =>{
             if(isNativeContext && window){
-                window.location.href = WEBAPP_READY_ACTION_URL;
+                window.location.href = LEGACY_COMMANDS.WEBAPP_READY_ACTION_URL;
             }
         },
 
         "leaveWebApp":() => {
             if(isNativeContext && window){
-                window.location.href = CLOSE_ACTION_URL;
+                window.location.href = CLOSE_ACTION_URL
             }
         }
 
     }
+
+    window.nativemainapp={nativebridge : bridge}
+    return bridge
 
 }()
